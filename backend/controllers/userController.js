3178f25026b011a1b155edb6c38d09a7b3975c6d
GUchechi <godswilluchendu7@gmail.com>
2023-06-29 15:59:16 +0100

    getUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,26 @@ import asyncHandler from "express-async-handler";
 import User from "../models/userModel.js";
 import generateToken from "../utils/generateToken.js";
 
+// @desc    Auth user/set token
+// route    POST /api/users/auth
+// @access  Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid email or password");
+  }
+});
 
 // @desc    Register new user
 // route    POST /api/users/
