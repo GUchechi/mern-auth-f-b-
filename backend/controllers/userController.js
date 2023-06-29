db89f882ff95746163b1e769a1f2bf67ccb373b4
GUchechi <godswilluchendu7@gmail.com>
2023-06-29 16:00:13 +0100

    logout

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -55,7 +55,17 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc   Logout
+// route    POST /api/users/logout
+// @access  Public
+const logoutUser = asyncHandler(async (req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
 
+  res.status(200).json({ message: "User logged out successfully" });
+});
 
 // @desc    Get user profile
 // route    GET /api/users/profile
