b08e91878d35f6d56ac71281939956f78aecc7e7
GUchechi <godswilluchendu7@gmail.com>
2023-06-29 15:54:37 +0100

    errorMiddleware

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,9 +1,14 @@
+// Catch-all for any routes that don't exist.
+
 const notFound = (req, res, next) => {
   const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
   next(error);
 };
 
+
+
+// Catch-all for any errors that occur in our routes.
 const errorHandler = (err, req, res, next) => {
   let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   let message = err.message;
