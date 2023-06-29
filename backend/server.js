8d7be3640ef9d3cc377df9f7383646616f458290
GUchechi <godswilluchendu7@gmail.com>
2023-06-29 14:30:48 +0100

    backend

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,18 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 app.use("/api/users", userRoutes);
 
