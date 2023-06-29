05e35d55f6533622578f64c8daa8d89253c9c756
GUchechi <godswilluchendu7@gmail.com>
2023-06-29 16:04:28 +0100

    routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,12 +10,6 @@ import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/', registerUser);
-router.post('/auth', authUser);
-router.post('/logout', logoutUser);
-router
-  .route('/profile')
-  .get(protect, getUserProfile)
-  .put(protect, updateUserProfile);
+
 
 export default router;
