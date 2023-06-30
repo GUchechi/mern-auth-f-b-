3c6d69d24c34a193b17668212aca99201e9765ff
GUchechi <godswilluchendu7@gmail.com>
2023-06-30 01:59:58 +0100

    auth

--- frontend/src/slices/authSlice.js
+++ frontend/src/slices/authSlice.js
@@ -9,7 +9,11 @@ const initialState = {
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: 
+  reducers: {
+    setCredentials: (state, action) => {
+      state.userInfo = action.payload;
+      localStorage.setItem("userInfo", JSON.stringify(action.payload));
+    },
     logout: (state, action) => {
       state.userInfo = null;
       localStorage.removeItem("userInfo");
