4f09303fc8d817c244eb3f5d0cf722969eaa2c90
GUchechi <godswilluchendu7@gmail.com>
2023-07-01 07:39:12 +0100

    refactoring

--- backend/server.js
+++ backend/server.js
@@ -33,8 +33,6 @@ app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => res.send("server is ready!!!"));
 
-// From errorMiddleware
-app.use(notFound);
-app.use(errorHandler);
+
 
 app.listen(port, () => console.log(`Server stared on post ${port}`));
