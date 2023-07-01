e3c85c1bed039587caa83793bde1b662238616c9
GUchechi <godswilluchendu7@gmail.com>
2023-07-01 07:28:55 +0100

    header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -47,7 +47,16 @@ const Header = () => {
                 </>
               ) : (
                 <>
-                 
+                  <LinkContainer to="/login">
+                    <Nav.Link>
+                      <FaSignInAlt /> Sign In
+                    </Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to="/register">
+                    <Nav.Link>
+                      <FaSignOutAlt /> Sign Up
+                    </Nav.Link>
+                  </LinkContainer>
                 </>
               )}
             </Nav>
