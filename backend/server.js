601499fb8f676ea476e7211e358ae47d3b5c3e71
GUchechi <godswilluchendu7@gmail.com>
2023-07-01 07:39:28 +0100

    refactoring

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,10 @@ import userRoutes from "./routes/userRoutes.js";
 
 connectDB();
 
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
