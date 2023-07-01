7fbfc8f327f7ec276b9c00e1a36cb4e51bf5ecaa
GUchechi <godswilluchendu7@gmail.com>
2023-07-01 07:29:38 +0100

    login

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -3,7 +3,12 @@ const USERS_URL = "/api/users";
 
 export const userApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-   
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/auth`,
+        method: "POST",
+        body: data,
+      }),
     }),
     logout: builder.mutation({
       query: () => ({
